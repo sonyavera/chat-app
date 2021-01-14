3a7026ac46fe73a05ac12817cc3623c640a8eb68
sonyavera <sonyavgould@gmail.com>
2021-01-14 11:40:30 -0700

    initialized firebase proj

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import 'rsuite/dist/styles/rsuite-default.css';
+import './styles/main.scss'
 
 function App() {
   return (
---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+const config = {
+
+    apiKey: "AIzaSyAvke3VU70DBiLRXxR4ctoOFyc5PHNATj8",
+    authDomain: "chat-web-app-d5e25.firebaseapp.com",
+    projectId: "chat-web-app-d5e25",
+    storageBucket: "chat-web-app-d5e25.appspot.com",
+    messagingSenderId: "269592647500",
+    appId: "1:269592647500:web:e87a41768817d491b221e9"
+};
+
+const app = firebase.initializeApp(config);
+
