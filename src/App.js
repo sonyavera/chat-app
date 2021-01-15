61f35bb330cbd38674c45908afdef0c7b3abbb7f
sonyavera <sonyavgould@gmail.com>
2021-01-14 17:49:04 -0700

    private and public routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import 'rsuite/dist/styles/rsuite-default.css';
+// import './styles/rsuite-default.css';
 import './styles/main.scss'
+import { Switch } from 'react-router'
+import SignIn from './pages/SignIn';
+import Home from './pages/Home'
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
-  return (
-    <div>
-      hello
-    </div>
-  );
+  return ( 
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn/>
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home/>
+      </PrivateRoute>
+    </Switch>
+    )
 }
 
 export default App;
